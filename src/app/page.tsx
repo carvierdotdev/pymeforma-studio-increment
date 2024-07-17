f2040897bd3be7610a230aecf088d0ace40b2ded
Carvier <60159324+carvierdotdev@users.noreply.github.com>
2024-07-17 19:12:35 -0400

    feat: add Navbar component and update Home page
    •  Added Navbar component to src/components/website/Navbar.tsx using Flowbite-React
    •  Updated Home page to include Navbar component
    
    commit-id:7c588b91

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import CallToActionButton from "@/components/website/CallToActionButton";
 import Hero from "@/components/website/Hero";
+import Navbar from "@/components/website/Navbar";
 import SplitButtonLeft from "@/components/website/SplitButtonLeft";
 import SplitImageLeft from "@/components/website/SplitImageLeft";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Hero />
       <CallToActionButton />
       <SplitImageLeft />
---
+++ src/components/website/Navbar.tsx
@@ -0,0 +1,40 @@
+import {
+  Navbar as FlowbiteNavbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import Link from "next/link";
+
+interface Props {}
+
+const Navbar = () => {
+  return (
+    <FlowbiteNavbar fluid={true} rounded={true} className="xl:px-12">
+      <NavbarBrand as={Link} href="https://pymeforma.com">
+        <img
+          src="/FrameLogo.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Pymeforma Studio Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Pymeforma Studio
+        </span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <NavbarLink href="#hero-text" active>
+          Inicio
+        </NavbarLink>
+        <NavbarLink as={Link} href="#cta-text">
+          Sobre nosotros
+        </NavbarLink>
+        <NavbarLink href="#social-media-text">Redes Sociales</NavbarLink>
+        <NavbarLink href="#website-text">Sitios web</NavbarLink>
+      </NavbarCollapse>
+    </FlowbiteNavbar>
+  );
+};
+
+export default Navbar;
